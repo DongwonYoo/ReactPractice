6232e78d22117610b8dfb7bcd819dc21fda8d6e5
Yoo DongWon <thd301302@gmail.com>
2023-05-18 11:22:56 +0900

    toDos

---
+++ pre/todo/todos/src/App.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const App = () => {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault(); // 再読み込みう防止
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  return (
+    <div>
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        ></input>
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos
+          .slice(0)
+          .reverse()
+          .map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
